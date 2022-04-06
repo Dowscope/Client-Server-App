0a266a019dcfdac36b4cf8e18e9b36740252d783
Timothy Dowling <timothy.dowling@me.com>
2022-04-06 15:52:53 -0400

    Update to client

--- client/index.js
+++ client/index.js
@@ -1,7 +1,13 @@
 
+// Connect to the server
 const socket = io('localhost:53002')
 socket.on('connect', () => {
 })
+
+// GameState Object
+const gameState = {
+  drops: []
+}
 
 function drop() {
   socket.emit('drop')
@@ -19,5 +25,40 @@ function registerName() {
 }
 
 function createDrop() {
-  const element = '<div class="drop"><img src="./images/rabbit.png"></div>'
+  const element = createDropElement()
+  const drop = {
+    id: Date.now() + Math.random(),
+    element,
+    location: {
+      x: window.innerWidth * Math.random(),
+      y: -200,
+    },
+    velocity: {
+        x: Math.random() * (Math.random() > 0.5 ? -1 : 1) * 3,
+        y: 1 + Math.random() * 2,
+    },
+    landed: false,
+  }
+  drops.push(drop)
+  document.body.appendChild(element);
+  updateElementLocation(drop);
 }
+
+function createDropElement()
+{
+    const element = document.createElement('div');
+    element.className = 'drop';
+    element.innerHTML = `
+        <img class="icon" src="./images/rabbit.png">
+    `
+    return element;
+}
+
+function updateElementLocation(drop) {
+  drop.element.style.top = drop.location.y + 'px';
+  drop.element.style.left = drop.location.x - (drop.element.clientWidth / 2) + 'px';
+}
+
+
+
+init()
